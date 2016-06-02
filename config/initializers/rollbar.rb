9a4d8a5ad7360e661c4112d5e225923ad4b6c15a
Oleg Sulyanov <oleg@sulyanov.com>
2016-06-02 17:34:14 +0700

    Fix code style

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -24,8 +24,10 @@ Rollbar.configure do |config|
   # 'ignore' will cause the exception to not be reported at all.
   # config.exception_level_filters.merge!('MyCriticalException' => 'critical')
   #
-  # You can also specify a callable, which will be called with the exception instance.
-  # config.exception_level_filters.merge!('MyCriticalException' => lambda { |e| 'critical' })
+  # You can also specify a callable, which will be called with the exception
+  # instance.
+  # config.exception_level_filters
+  #       .merge!('MyCriticalException' => lambda { |e| 'critical' })
 
   # Enable asynchronous reporting (uses girl_friday or Threading if girl_friday
   # is not installed)
@@ -47,6 +49,6 @@ Rollbar.configure do |config|
   # you'll want to override the environment reported by `Rails.env` with an
   # environment variable like this: `ROLLBAR_ENV=staging`. This is a recommended
   # setup for Heroku. See:
-  # https://devcenter.heroku.com/articles/deploying-to-a-custom-rails-environment
+  # http://devcenter.heroku.com/articles/deploying-to-a-custom-rails-environment
   config.environment = ENV['ROLLBAR_ENV'] || Rails.env
 end if Rails.env.production?
