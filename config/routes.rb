7752bbe8b6ba293d61f7930bf0a136bfe5054283
Oleg Sulyanov <oleg@sulyanov.com>
2016-06-06 22:30:58 +0700

    Add routes for Registrations controller and apipie

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   root 'home#index'
+
+  apipie
   use_doorkeeper
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  namespace :api, defaults: { format: 'json' } do
+    scope module: :v1, path: 'v1' do
+      devise_scope :user do
+        post '/registrations' => 'registrations#create'
+      end
+    end
+  end
 end
