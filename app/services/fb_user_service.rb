eac0bf810bad1b59b4c41df9705322f871d1b231
Oleg Sulyanov <oleg@sulyanov.com>
2016-06-07 17:03:09 +0700

    Add FbUserService service

---
+++ app/services/fb_user_service.rb
@@ -0,0 +1,41 @@
+class FbUserService
+  def self.find_or_create_by_facebook_id(data)
+    user = get_by_id(data['id'])
+    return user if user
+
+    user = get_by_email(data['email'], data['id'])
+    return user if user
+
+    create_new(data)
+  end
+
+  def self.get_by_id(fb_id)
+    User.find_by(facebook_id: fb_id)
+  end
+
+  def self.get_by_email(email, fb_id)
+    user = User.find_by(email: email)
+    if user
+      user.update_attribute :facebook_id, fb_id
+      return user
+    end
+  end
+
+  def self.create_new(data)
+    user = User.create!(facebook_id: data['id'], bio: data['bio'],
+                dob_on: dob(data['birthday']), email: data['email'],
+                name: data['first_name'], surname: data['last_name'],
+                sex: sex(data['gender']), active: true,
+                 password: Devise.friendly_token.first(6))
+    user.send_welcome_message if user.persisted?
+    user
+  end
+
+  def self.dob(date)
+    date.present? ? Date.strptime(date, '%m/%d/%Y') : nil
+  end
+
+  def self.sex(sex)
+    User.sexes.include?(sex) ? sex : nil
+  end
+end
