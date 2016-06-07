66b9bdf3a8115920e0262384b7f6867fca58537d
Oleg Sulyanov <oleg@sulyanov.com>
2016-06-07 17:50:18 +0700

    Check archived flash on sign in via FB token

--- app/services/fb_user_service.rb
+++ app/services/fb_user_service.rb
@@ -1,12 +1,9 @@
 class FbUserService
   def self.find_or_create_by_facebook_id(data)
     user = get_by_id(data['id'])
-    return user if user
-
-    user = get_by_email(data['email'], data['id'])
-    return user if user
-
-    create_new(data)
+    user = get_by_email(data['email'], data['id']) unless user
+    user = create_new(data) unless user
+    user.persisted? && !user.archived? ? user : nil
   end
 
   def self.get_by_id(fb_id)
@@ -23,10 +20,10 @@ class FbUserService
 
   def self.create_new(data)
     user = User.create!(facebook_id: data['id'], bio: data['bio'],
-                dob_on: dob(data['birthday']), email: data['email'],
-                name: data['first_name'], surname: data['last_name'],
-                sex: sex(data['gender']), active: true,
-                 password: Devise.friendly_token.first(6))
+                        dob_on: dob(data['birthday']), email: data['email'],
+                        name: data['first_name'], surname: data['last_name'],
+                        sex: sex(data['gender']), active: true,
+                        password: Devise.friendly_token.first(6))
     user.send_welcome_message if user.persisted?
     user
   end
