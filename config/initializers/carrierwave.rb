3250cc1c9636f0e3003c9651aca2936f7b737c41
Oleg Sulyanov <oleg@sulyanov.com>
2016-06-08 18:46:00 +0700

    Small fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,13 +7,13 @@ CarrierWave.configure do |config|
       config.fog_provider = 'fog/aws'
       config.fog_credentials = {
           provider: 'AWS',
-          aws_access_key_id: Rails.secrets.aws_access_key_id,
-          aws_secret_access_key: Rails.secrets.aws_secret_access_key,
+          aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+          aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
           # region:                'eu-west-1',
           # host:                  's3.example.com',
           # endpoint:              'https://s3.example.com:8080'
       }
-      config.fog_directory = Rails.secrets.aws_bucket
+      config.fog_directory = Rails.application.secrets.aws_bucket
       config.fog_public = true
       config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
   end
