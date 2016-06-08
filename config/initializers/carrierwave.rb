75c047d3e0e89623f4496e32616e2d488c47b821
Oleg Sulyanov <oleg@sulyanov.com>
2016-06-08 18:44:18 +0700

    Move AWS credentials to the secrets.yml

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,13 +7,13 @@ CarrierWave.configure do |config|
       config.fog_provider = 'fog/aws'
       config.fog_credentials = {
           provider: 'AWS',
-          aws_access_key_id: 'xxx',
-          aws_secret_access_key: 'yyy',
+          aws_access_key_id: Rails.secrets.aws_access_key_id,
+          aws_secret_access_key: Rails.secrets.aws_secret_access_key,
           # region:                'eu-west-1',
           # host:                  's3.example.com',
           # endpoint:              'https://s3.example.com:8080'
       }
-      config.fog_directory = 'name_of_directory'
+      config.fog_directory = Rails.secrets.aws_bucket
       config.fog_public = true
       config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
   end
