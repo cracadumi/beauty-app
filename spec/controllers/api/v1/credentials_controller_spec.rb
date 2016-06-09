fd694e72a34d625eadeaee45dacd8ecbac9450b3
Oleg Sulyanov <oleg@sulyanov.com>
2016-06-09 18:47:23 +0700

    Add tests for Credentials#Destroy API

--- spec/controllers/api/v1/credentials_controller_spec.rb
+++ spec/controllers/api/v1/credentials_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Api::V1::CredentialsController, type: :controller do
-  describe 'GET #show (integrated)' do
+  describe 'GET #show' do
     let(:user) { create :user }
     let(:token) { create :access_token, resource_owner_id: user.id }
 
@@ -27,12 +27,12 @@ describe Api::V1::CredentialsController, type: :controller do
     end
   end
 
-  describe 'PUT #update (integrated)' do
+  describe 'PUT #update' do
     let(:user) { create :user }
     let(:token) { create :access_token, resource_owner_id: user.id }
 
     before do
-      put :update, format: :json, access_token: token.token, id: user.id,
+      put :update, format: :json, access_token: token.token,
                    user: { name: 'UpdatedName' }
     end
 
@@ -52,6 +52,37 @@ describe Api::V1::CredentialsController, type: :controller do
       it { expect(response).to have_http_status(:unauthorized) }
 
       it { expect(response.body).to be_empty }
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let(:user) { create :user }
+    let(:token) { create :access_token, resource_owner_id: user.id }
+
+    before do
+      delete :destroy, format: :json, access_token: token.token,
+                   user: { name: 'UpdatedName' }
+    end
+
+    it { expect(response).to have_http_status(:success) }
+
+    it 'archives user' do
+      user.reload
+      expect(user).to be_archived
+    end
+
+    it 'revokes user\'s token' do
+      token.reload
+      expect(token).to be_revoked
+    end
+
+    context 'with expired access_token' do
+      let(:token) do
+        create :access_token, resource_owner_id: user.id,
+                              expires_in: 0
+      end
+
+      it { expect(response).to have_http_status(:unauthorized) }
     end
   end
 end
