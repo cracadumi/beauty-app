c092673c0ec3e8ce2e3ce95e0d1be6b9bbb44181
Oleg Sulyanov <oleg@sulyanov.com>
2016-06-14 20:17:00 +0700

    Add sub_category admin resource

---
+++ app/admin/sub_category.rb
@@ -0,0 +1,22 @@
+ActiveAdmin.register SubCategory do
+  permit_params :category_id, :name
+
+  index do
+    selectable_column
+    id_column
+    column :category
+    column :name
+    actions
+  end
+
+  filter :category
+  filter :name
+
+  form do |f|
+    f.inputs 'SubCategory Details' do
+      f.input :category
+      f.input :name
+    end
+    f.actions
+  end
+end
