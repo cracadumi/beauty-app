78618e3295defd1a62ec8a33bdad4b42bebe084e
Oleg Sulyanov <oleg@sulyanov.com>
2016-06-21 17:14:26 +0700

    Add Picture admin resource

---
+++ app/admin/picture.rb
@@ -0,0 +1,29 @@
+ActiveAdmin.register Picture do
+  permit_params :picturable_id, :picturable_type, :title, :description,
+                :picture_url
+
+  index do
+    selectable_column
+    id_column
+    column :picturable
+    column(:picture) do |a|
+      image_tag a.picture_url, height: 30 if a.picture_url
+    end
+    column :title
+    actions
+  end
+
+  filter :picturable, as: :select, collection: Picture.all.map(&:picturable)
+
+  form do |f|
+    f.inputs 'Address Details' do
+      f.semantic_errors(*f.object.errors.keys)
+      f.input :picturable_id
+      f.input :picturable_type
+      f.input :title
+      f.input :description
+      f.input :picture_url
+    end
+    f.actions
+  end
+end
