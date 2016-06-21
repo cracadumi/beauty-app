23c5618b5e55380d97137210149a95112ba842ad
Oleg Sulyanov <oleg@sulyanov.com>
2016-06-21 17:20:26 +0700

    Add Review admin resource

---
+++ app/admin/review.rb
@@ -0,0 +1,32 @@
+ActiveAdmin.register Review do
+  permit_params :booking_id, :user_id, :rating, :comment, :author_id, :visible
+
+  index do
+    selectable_column
+    id_column
+    column :booking
+    column :user
+    column :author
+    column :rating
+    column :visible
+    actions
+  end
+
+  filter :booking
+  filter :user
+  filter :author
+  filter :visible
+
+  form do |f|
+    f.inputs 'Review Details' do
+      f.semantic_errors(*f.object.errors.keys)
+      f.input :booking
+      f.input :user
+      f.input :rating
+      f.input :comment
+      f.input :author
+      f.input :visible
+    end
+    f.actions
+  end
+end
