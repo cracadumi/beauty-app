e1ec76f35ab92f506fee560ebb8c80083d499733
Oleg Sulyanov <oleg@sulyanov.com>
2016-06-23 17:41:43 +0700

    Refactor SettingsBeauticiansController

--- app/controllers/api/v1/settings_beauticians_controller.rb
+++ app/controllers/api/v1/settings_beauticians_controller.rb
@@ -3,46 +3,10 @@ module Api
     class SettingsBeauticiansController < Api::V1::V1Controller
       before_action :doorkeeper_authorize!
       load_and_authorize_resource
-      before_action :set_my_settings_beautician, only: [:me]
       before_action :set_settings_beautician, only: [:show]
 
       resource_description do
         short 'SettingsBeauticians'
-      end
-
-      api :GET, '/v1/settings_beauticians/me', 'Show My SettingsBeauticians'
-      description <<-EOS
-        ## Description
-        Show SettingsBeautician data of current user
-      EOS
-      example <<-EOS
-        {
-          "instant_booking": true,
-          "advance_booking": true,
-          "mobile": true,
-          "office": true,
-          "office_address": {
-            "street": "123",
-            "postcode": 123,
-            "city": "123",
-            "state": "",
-            "latitude": 37.5700969,
-            "longitude": 126.9913845,
-            "country": "FR"
-          },
-          "availabilities": [
-            {
-              "day": "sunday",
-              "starts_at_time": "09:00",
-              "ends_at_time": "17:00",
-              "working_day": true
-            }
-          ]
-        }
-      EOS
-
-      def me
-        respond_with @settings_beautician
       end
 
       api :GET, '/v1/settings_beauticians/:id', 'Show SettingsBeauticians'
@@ -82,13 +46,8 @@ module Api
 
       private
 
-      def set_my_settings_beautician
-        @user = current_user
-        @settings_beautician = @user.settings_beautician
-      end
-
       def set_settings_beautician
-        @user = User.find_by(id: params[:id])
+        @user = params[:id] ? User.find_by(id: params[:id]) : current_user
         @settings_beautician = @user.settings_beautician
       end
     end
