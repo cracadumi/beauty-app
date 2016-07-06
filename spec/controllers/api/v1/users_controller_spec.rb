9c4bc6c386c7f12d01d14bdeede435267f347d89
Oleg Sulyanov <oleg@sulyanov.com>
2016-07-06 11:02:23 +0700

    Add simple test for Users#Index API

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,6 +1,28 @@
 require 'rails_helper'
 
 describe Api::V1::UsersController, type: :controller do
+  describe 'GET #index' do
+    let!(:beautician) do
+      create :user, role: :beautician, active: true,
+             last_tracked_at: 1.minute.ago, latitude: 1.11, longitude: 2.22
+    end
+    let(:user) { create :user }
+    let(:token) { create :access_token, resource_owner_id: user.id }
+
+    before do
+      get :index, format: :json, access_token: token.token,
+          latitude: 1.21, longitude: 2.22
+    end
+
+    it { expect(response).to have_http_status(:success) }
+
+    it 'returns languages as JSON' do
+      result = json.map { |r| r['id'] }
+
+      expect(result).to eq([beautician.id])
+    end
+  end
+
   describe 'GET #show' do
     before do
       get :show, format: :json, id: user.id, access_token: token.token
