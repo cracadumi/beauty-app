336a3074c6cd53e94539b30b45431c99c63e4ea7
Oleg Sulyanov <oleg@sulyanov.com>
2016-08-15 13:12:10 +0700

    Add test users and their credentials to README

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,9 @@
-User.create!(email: 'admin@example.com', password: '1q2w3e4r', name: 'Admin',
-             surname: '–', role: :admin, password_confirmation: '1q2w3e4r')
+User.create(email: 'admin@example.com', password: '1q2w3e4r', name: 'Admin',
+            surname: '–', role: :user, password_confirmation: '1q2w3e4r',
+            username: 'admin')
+User.create(email: 'user@example.com', password: '1q2w3e4r', name: 'User',
+            surname: 'Test', role: :admin, password_confirmation: '1q2w3e4r',
+            username: 'user')
+User.create(email: 'beautician@example.com', password: '1q2w3e4r',
+            name: 'Beautician', surname: 'Test', role: :beautician,
+            password_confirmation: '1q2w3e4r', username: 'beautician')
